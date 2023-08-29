2be18cb1c14126759c452386aecde6530aaa98b6
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-08-29 18:43:43 +0300

    solution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,11 @@
 package org.example;
+
 public class Main {
     public static void main(String[] args) {
+        int centimeters = 243;
+
+        int meters = centimeters / 100;
+
+        System.out.println(meters+"m");
     }
 }
